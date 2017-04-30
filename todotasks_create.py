4f306c9505b71de764eadc26028fb2a8a0afe1f4
Loris Tissino <loris.tissino@gmail.com>
2017-04-30 18:05:37 +0200

    Added explicit commit

--- todotasks_create.py
+++ todotasks_create.py
@@ -9,6 +9,11 @@ TodoTask = self.env['todo.task']
 # retrieves a Recordset Model for the TodoTask class
 
 for i in range(10):
-    newtask = TodoTask.create( { 'name': 'New task %03d' % i } )
-    newtask.is_done = i % 2 == 0
+    is_done = i % 2 == 0
     # we create half of the tasks as done
+    newtask = TodoTask.create( { 'name': 'New task %03d' % i, 'is_done': is_done } )
+    print "created task, id=", newtask.id
+
+# self.env.cr.commit()
+# to make the changes persistent, uncomment the preceding line,
+# that should be the last instruction executed
